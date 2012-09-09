ca424e91e075907d6bc929fca33f6512eda418f5
Sebastian Hoß <mail@shoss.de>
2012-09-09 19:14:26 +0200

    Contracts are now inherited

--- annotations/src/main/java/com/github/sebhoss/contract/annotation/Contract.java
+++ annotations/src/main/java/com/github/sebhoss/contract/annotation/Contract.java
@@ -7,11 +7,14 @@ package com.github.sebhoss.contract.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
+@Inherited
+// TODO: Gibt es Probleme mit Vererbung?
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Contract {
