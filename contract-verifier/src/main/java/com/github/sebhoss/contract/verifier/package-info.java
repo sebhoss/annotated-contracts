540044e96e1c3a0b0d3937e2415ee32595384633
Sebastian Hoß <mail@shoss.de>
2013-08-10 21:58:20 +0200

    Added package documentation
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-verifier/src/main/java/com/github/sebhoss/contract/verifier/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
+/**
+ * Verifier verify a method contract.
+ */
+@NotNullByDefault
+package com.github.sebhoss.contract.verifier;
+
+import com.github.sebhoss.common.annotation.NotNullByDefault;
+
