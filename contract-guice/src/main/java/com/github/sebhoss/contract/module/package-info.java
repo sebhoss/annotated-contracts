48f842df243617c86560a42eb341eef92844bb00
Sebastian Hoß <mail@shoss.de>
2013-08-30 22:35:02 +0200

    Added default nonnull-behavior
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-guice/src/main/java/com/github/sebhoss/contract/module/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+/**
+ * Guice modules for annotated-contracts.
+ */
+@NotNullByDefault
+package com.github.sebhoss.contract.module;
+
+import com.github.sebhoss.common.annotation.NotNullByDefault;
+
