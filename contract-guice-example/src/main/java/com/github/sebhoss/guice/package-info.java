d57f775483e3a84f3775b6ffff15d8226e9fd70a
Sebastian Hoß <mail@shoss.de>
2013-08-30 22:42:28 +0200

    Added default nonnull-analysis
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-guice-example/src/main/java/com/github/sebhoss/guice/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+/**
+ * Sample implementation of a domain model which uses annotation-based contracts.
+ */
+@NotNullByDefault
+package com.github.sebhoss.guice;
+
+import com.github.sebhoss.common.annotation.NotNullByDefault;
+
