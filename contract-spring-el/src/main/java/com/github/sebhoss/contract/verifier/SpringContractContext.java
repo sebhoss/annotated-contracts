db1abf4e4700aed4c323411302af1321ab0548d0
Sebastian Hoß <mail@shoss.de>
2013-09-05 00:41:55 +0200

    Added Spring-EL implementation + Guice module
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-spring-el/src/main/java/com/github/sebhoss/contract/verifier/SpringContractContext.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier;
+
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+
+import com.github.sebhoss.contract.annotation.Clause;
+
+/**
+ * Spring-based implementation of the {@link ContractContext}.
+ */
+public class SpringContractContext implements ContractContext {
+
+    private final ExpressionParser  parser;
+    private final EvaluationContext context;
+
+    SpringContractContext(final ExpressionParser parser, final EvaluationContext context) {
+        this.parser = parser;
+        this.context = context;
+    }
+
+    @Override
+    public void setInvocationResult(final Object invocationResult) {
+        context.setVariable(Clause.RETURN, invocationResult);
+    }
+
+    @Override
+    public boolean isInViolationWith(final Clause clause) {
+        final Expression expression = parser.parseExpression(clause.value());
+
+        return Boolean.FALSE.equals(expression.getValue(context, Boolean.class));
+    }
+
+}
---
+++ contract-spring-el/src/main/java/com/github/sebhoss/contract/verifier/SpringELBasedContractContextFactory.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier;
+
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import com.github.sebhoss.contract.annotation.Clause;
+
+/**
+ * Spring-based implementation of the {@link ContractContextFactory}.
+ */
+public class SpringELBasedContractContextFactory implements ContractContextFactory {
+
+    @Override
+    public ContractContext createContext(final Object instance, final Object[] arguments, final String[] parameterNames) {
+        final ExpressionParser parser = new SpelExpressionParser();
+        final EvaluationContext context = new StandardEvaluationContext();
+
+        for (int index = 0; index < arguments.length; index++) {
+            context.setVariable(parameterNames[index], arguments[index]);
+        }
+        context.setVariable(Clause.THIS, instance);
+
+        return new SpringContractContext(parser, context);
+    }
+
+}
