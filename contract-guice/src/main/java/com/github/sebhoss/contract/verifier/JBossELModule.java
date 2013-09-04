4284725dadaeb6e99a97c0f414f1b7b00c271165
Sebastian Hoß <mail@shoss.de>
2013-09-04 22:01:41 +0200

    Added JBoss-EL module + Guice configuration
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-guice/src/main/java/com/github/sebhoss/contract/verifier/JBossELModule.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier;
+
+import javax.el.ExpressionFactory;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Guice module which configures JBoss-EL for contract clause validation.
+ */
+public class JBossELModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        this.bind(ExpressionFactory.class).toInstance(ExpressionFactory.newInstance());
+        this.bind(ContractContextFactory.class).to(JBossBasedContractContextFactory.class);
+    }
+
+}
