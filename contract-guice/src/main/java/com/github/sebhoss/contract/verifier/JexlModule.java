eedf13d24de54423fb3729aaa03ca2cb91f81e9e
Sebastian Hoß <mail@shoss.de>
2013-09-04 22:46:11 +0200

    Added missing JEXL module for Guice
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-guice/src/main/java/com/github/sebhoss/contract/verifier/JexlModule.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Guice module which configures JEXL for contract clause validation.
+ */
+public class JexlModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        this.bind(ContractContextFactory.class).to(JEXLBasedContractContextFactory.class);
+    }
+
+}
