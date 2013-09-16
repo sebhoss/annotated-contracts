ec21064d9906aa597a31a47c787de6fc1f21c998
Sebastian Hoß <mail@shoss.de>
2013-09-16 23:27:02 +0200

    Added Guice-UEL example
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-examples/contract-guice-uel-example/src/main/java/com/github/sebhoss/contract/example/UelBasedInsuranceCompany.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.example;
+
+import com.github.sebhoss.contract.annotation.Clause;
+import com.github.sebhoss.contract.annotation.Contract;
+
+class UelBasedInsuranceCompany implements InsuranceCompany {
+
+    @Contract(preconditions = {
+            @Clause(value = "${damage > 0}", message = "Reported damage must be positive!", exception = IllegalStateException.class),
+            @Clause("${damage <= 5000}") }, postconditions = { @Clause("${return >= 0}"), @Clause("${return <= 2000}") })
+    @Override
+    public double calculateCover(final double damage) {
+        return damage * 0.5;
+    }
+
+}
