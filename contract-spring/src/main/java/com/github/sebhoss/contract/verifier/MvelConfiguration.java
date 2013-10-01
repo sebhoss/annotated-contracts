746dfcd6ea81e5a368a08dd4267dc0a8a230dc9b
Sebastian Hoß <mail@shoss.de>
2013-10-01 22:33:35 +0200

    Added Spring-MVEL example
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-spring/src/main/java/com/github/sebhoss/contract/verifier/MvelConfiguration.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for a MVEL based {@link ContractContextFactory}.
+ */
+@Configuration
+public class MvelConfiguration {
+
+    /**
+     * @return A MVEL based {@link ContractContextFactory}.
+     */
+    @Bean
+    @SuppressWarnings(CompilerWarnings.STATIC_METHOD)
+    public ContractContextFactory contractContextFactory() {
+        return new MVELBasedContractContextFactory();
+    }
+
+}
