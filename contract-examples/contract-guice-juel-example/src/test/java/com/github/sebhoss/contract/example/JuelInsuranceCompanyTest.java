ef6971ad4ebc3540595e5b31da94f24cfb732faf
Sebastian Hoß <mail@shoss.de>
2013-10-01 15:09:22 +0200

    Introduced @Before
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- contract-examples/contract-guice-juel-example/src/test/java/com/github/sebhoss/contract/example/JuelInsuranceCompanyTest.java
+++ contract-examples/contract-guice-juel-example/src/test/java/com/github/sebhoss/contract/example/JuelInsuranceCompanyTest.java
@@ -6,38 +6,47 @@
  */
 package com.github.sebhoss.contract.example;
 
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import javax.annotation.Nullable;
 
 import com.github.sebhoss.common.annotation.CompilerWarnings;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 /**
  * Test cases for the {@link InsuranceCompany}.
  */
-@SuppressWarnings({ CompilerWarnings.NLS, CompilerWarnings.NULL, CompilerWarnings.STATIC_METHOD })
+@SuppressWarnings({ CompilerWarnings.NLS, CompilerWarnings.NULL })
 public class JuelInsuranceCompanyTest {
 
     /** Catches expected exceptions */
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
+    @Nullable
+    private InsuranceCompany insurance;
+
+    /**
+     * Creates a new insurance company for each test.
+     */
+    @Before
+    public void createCompany() {
+        final Injector injector = Guice.createInjector(new CompanyModule());
+        insurance = injector.getInstance(InsuranceCompany.class);
+    }
+
     /**
      * Ensures that the insurance company can calculate payouts based on a positive damage input.
      */
     @Test
     public void shouldAcceptPositiveDamages() {
-        // Given
-        final Injector injector = Guice.createInjector(new CompanyModule());
-        final InsuranceCompany instance = injector.getInstance(InsuranceCompany.class);
+        final double result = insurance.calculateCover(10);
 
-        // When
-        final double result = instance.calculateCover(10);
-
-        // Then
         Assert.assertEquals(5.0, result, 0d);
     }
 
@@ -46,16 +55,10 @@ public class JuelInsuranceCompanyTest {
      */
     @Test
     public void shouldNotAcceptNegativeDamages() {
-        // Given
-        final Injector injector = Guice.createInjector(new CompanyModule());
-        final InsuranceCompany instance = injector.getInstance(InsuranceCompany.class);
-
-        // When
         thrown.expect(IllegalStateException.class);
         thrown.expectMessage("Reported damage must be positive!");
 
-        // Then
-        instance.calculateCover(-10);
+        insurance.calculateCover(-10);
     }
 
     /**
@@ -63,15 +66,10 @@ public class JuelInsuranceCompanyTest {
      */
     @Test
     public void shouldNotAcceptHighDamages() {
-        // Given
-        final Injector injector = Guice.createInjector(new CompanyModule());
-        final InsuranceCompany instance = injector.getInstance(InsuranceCompany.class);
-
-        // When
         thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("We won't pay that!");
 
-        // Then
-        instance.calculateCover(5001);
+        insurance.calculateCover(5001);
     }
 
     /**
@@ -79,15 +77,10 @@ public class JuelInsuranceCompanyTest {
      */
     @Test
     public void shouldNotPerformHighPayout() {
-        // Given
-        final Injector injector = Guice.createInjector(new CompanyModule());
-        final InsuranceCompany instance = injector.getInstance(InsuranceCompany.class);
-
-        // When
         thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("We can't pay that much!");
 
-        // Then
-        instance.calculateCover(5000);
+        insurance.calculateCover(5000);
     }
 
 }
