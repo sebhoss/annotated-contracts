d5ce8bbcc1456c691c075554aa8d74fb298614fb
Sebastian Hoß <mail@shoss.de>
2013-10-01 21:43:36 +0200

    Renamed Spring -> SpringSpEL
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- contract-examples/contract-spring-spel-example/src/main/java/com/github/sebhoss/contract/example/CompanySpringConfiguration.java
+++ contract-examples/contract-spring-spel-example/src/main/java/com/github/sebhoss/contract/example/CompanySpringConfiguration.java
@@ -27,7 +27,7 @@ public class CompanySpringConfiguration {
     @Bean
     @SuppressWarnings(CompilerWarnings.STATIC_METHOD)
     public InsuranceCompany company() {
-        return new SpringBasedInsuranceCompany();
+        return new SpringSpELBasedInsuranceCompany();
     }
 
 }
--- contract-examples/contract-spring-spel-example/src/test/java/com/github/sebhoss/contract/example/SpringSpELInsuranceCompanyTest.java
+++ contract-examples/contract-spring-spel-example/src/test/java/com/github/sebhoss/contract/example/SpringSpELInsuranceCompanyTest.java
@@ -22,7 +22,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * Test cases for the {@link InsuranceCompany}.
  */
 @SuppressWarnings({ CompilerWarnings.NLS, CompilerWarnings.NULL, CompilerWarnings.RESOURCE })
-public class SpringInsuranceCompanyTest {
+public class SpringSpELInsuranceCompanyTest {
 
     /** Catches expected exceptions */
     @Rule
