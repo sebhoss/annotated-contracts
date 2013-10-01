a33648672a06d82cc1263b7d05666a02722a6f8e
Sebastian Hoß <mail@shoss.de>
2013-10-01 22:14:58 +0200

    Added Spring-JavaScript implementation
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-spring/src/main/java/com/github/sebhoss/contract/configuration/DefaultJavaScriptConfiguration.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.configuration;
+
+import com.github.sebhoss.contract.verifier.ContractConfiguration;
+import com.github.sebhoss.contract.verifier.ParameterNamesLookupConfiguration;
+import com.github.sebhoss.contract.verifier.ScriptEngineConfiguration;
+import com.github.sebhoss.contract.verifier.impl.ErrorMessageConfiguration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Default configuration for JEXL-based contracts.
+ */
+@Configuration
+@Import({ AspectConfiguration.class, ParameterNamesLookupConfiguration.class, ScriptEngineConfiguration.class,
+        ContractConfiguration.class, ErrorMessageConfiguration.class })
+public class DefaultJavaScriptConfiguration {
+
+    // Meta-configuration
+
+}
---
+++ contract-spring/src/main/java/com/github/sebhoss/contract/verifier/ScriptEngineConfiguration.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+import com.github.sebhoss.common.annotation.Nullsafe;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for a ScriptEngine based {@link ContractContextFactory}.
+ */
+@Configuration
+public class ScriptEngineConfiguration {
+
+    /**
+     * @return A JEXL based {@link ContractContextFactory}.
+     */
+    @Bean
+    public ContractContextFactory contractContextFactory() {
+        return new ScriptEngineBasedContractContextFactory(javaScriptEngine());
+    }
+
+    /**
+     * @return A JavaScript-based ScriptEngine.
+     */
+    @Bean
+    public ScriptEngine javaScriptEngine() {
+        return Nullsafe.nullsafe(scriptManager().getEngineByName("JavaScript")); //$NON-NLS-1$
+    }
+
+    /**
+     * @return A new ScriptEngineManager.
+     */
+    @Bean
+    @SuppressWarnings(CompilerWarnings.STATIC_METHOD)
+    public ScriptEngineManager scriptManager() {
+        return new ScriptEngineManager();
+    }
+
+}
