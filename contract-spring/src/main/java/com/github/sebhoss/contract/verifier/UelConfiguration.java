369a220e234b95821f78a746cd523ef68bb088bd
Sebastian Hoß <mail@shoss.de>
2013-10-01 22:46:32 +0200

    Added Spring-UEL implementation
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-spring/src/main/java/com/github/sebhoss/contract/verifier/UelConfiguration.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier;
+
+import javax.el.ExpressionFactory;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+import com.github.sebhoss.common.annotation.Nullsafe;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for a UEL based {@link ContractContextFactory}.
+ */
+@Configuration
+public class UelConfiguration {
+
+    /**
+     * @return A UEL based {@link ContractContextFactory}.
+     */
+    @Bean
+    public ContractContextFactory contractContextFactory() {
+        return new UelBasedContractContextFactory(expressionFactory());
+    }
+
+    /**
+     * @return A new ExpressionFactory.
+     */
+    @Bean
+    @SuppressWarnings(CompilerWarnings.STATIC_METHOD)
+    public ExpressionFactory expressionFactory() {
+        return Nullsafe.nullsafe(ExpressionFactory.newInstance());
+    }
+
+}
