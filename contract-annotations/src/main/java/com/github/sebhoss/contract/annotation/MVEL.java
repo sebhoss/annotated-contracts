a4c7a8922a213c2148912e6091de4acc9103df94
Sebastian Hoß <mail@shoss.de>
2013-10-03 20:04:12 +0200

    Added language annotation qualifiers
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-annotations/src/main/java/com/github/sebhoss/contract/annotation/MVEL.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+/**
+ * Marks a class as relevant to MVEL examples.
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.FIELD })
+public @interface MVEL {
+
+    // marker annotation
+
+}
