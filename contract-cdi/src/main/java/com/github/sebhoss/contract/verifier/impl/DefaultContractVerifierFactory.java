c308d3f354ddb653c5f34a6877d9d806e13c99f3
Sebastian Hoß <mail@shoss.de>
2013-10-03 21:03:58 +0200

    Added default ContractVerifierFactory configuration
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-cdi/src/main/java/com/github/sebhoss/contract/verifier/impl/DefaultContractVerifierFactory.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier.impl;
+
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Produces;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+import com.github.sebhoss.contract.verifier.ContractContextFactory;
+import com.github.sebhoss.contract.verifier.ContractExceptionFactory;
+import com.github.sebhoss.contract.verifier.ContractRetrieval;
+import com.github.sebhoss.contract.verifier.ContractSemanticCheck;
+import com.github.sebhoss.contract.verifier.ContractSyntaxCheck;
+import com.github.sebhoss.contract.verifier.ContractVerifierFactory;
+import com.github.sebhoss.contract.verifier.ParameterNamesLookup;
+
+/**
+ * Configures the default ContractVerifierFactory.
+ */
+public class DefaultContractVerifierFactory {
+
+    /**
+     * @param contractRetrieval
+     *            The contract retrieval to use.
+     * @param contractSyntaxCheck
+     *            The contract syntax checks to use.
+     * @param parameterNamesLookup
+     *            The parameter names lookup to use.
+     * @param contractSemanticCheck
+     *            The contract semantic checks to use.
+     * @param contractContextFactory
+     *            The contract context factory to use.
+     * @param contractExceptionFactory
+     *            The contract exception factory to use.
+     * @return Builds a context based contract verifier.
+     */
+    @Produces
+    @Default
+    @SuppressWarnings({ CompilerWarnings.STATIC_METHOD })
+    public ContractVerifierFactory contextBased(final ContractRetrieval contractRetrieval,
+            final ContractSyntaxCheck contractSyntaxCheck, final ParameterNamesLookup parameterNamesLookup,
+            final ContractSemanticCheck contractSemanticCheck, final ContractContextFactory contractContextFactory,
+            final ContractExceptionFactory contractExceptionFactory) {
+        return new ContextBasedContractVerifierFactory(contractRetrieval, contractSyntaxCheck, parameterNamesLookup,
+                contractSemanticCheck, contractContextFactory, contractExceptionFactory);
+    }
+
+}
