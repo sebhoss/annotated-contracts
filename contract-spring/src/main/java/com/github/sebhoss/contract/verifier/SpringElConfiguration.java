5732ad20e61bace2f1bcf4a69d65d327fd85d08c
Sebastian Hoß <mail@shoss.de>
2013-10-03 20:24:58 +0200

    Use new SpEL classes
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- contract-spring/src/main/java/com/github/sebhoss/contract/verifier/SpringElConfiguration.java
+++ contract-spring/src/main/java/com/github/sebhoss/contract/verifier/SpringElConfiguration.java
@@ -23,7 +23,7 @@ public class SpringElConfiguration {
     @Bean
     @SuppressWarnings(CompilerWarnings.STATIC_METHOD)
     public ContractContextFactory contractContextFactory() {
-        return new SpringELBasedContractContextFactory();
+        return new SpELBasedContractContextFactory();
     }
 
 }
