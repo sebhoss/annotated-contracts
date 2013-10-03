c723be2cc6f806faa4d1fdfbac3651f4a59a0842
Sebastian Hoß <mail@shoss.de>
2013-10-03 22:33:43 +0200

    Added CDI-JUEL example
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-examples/contract-cdi-juel-example/src/main/java/com/github/sebhoss/contract/example/CompanyConfiguration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.example;
+
+import java.util.Set;
+
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Produces;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+import com.github.sebhoss.common.annotation.Nullsafe;
+import com.github.sebhoss.contract.annotation.JUEL;
+import com.github.sebhoss.contract.verifier.ContractContextFactory;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.util.concurrent.Service;
+
+/**
+ * Configures a JUEL ContractContextFactory.
+ */
+public class CompanyConfiguration {
+
+    /**
+     * @param juelFactory
+     *            The JUEL context factory.
+     * @return A new context factory.
+     */
+    @Produces
+    @Default
+    @SuppressWarnings(CompilerWarnings.STATIC_METHOD)
+    public ContractContextFactory contextFactory(final @JUEL ContractContextFactory juelFactory) {
+        return juelFactory;
+    }
+
+    /**
+     * @return A set of services to manage. TODO: We only need this for pax-exam
+     */
+    @Produces
+    @Default
+    @SuppressWarnings(CompilerWarnings.STATIC_METHOD)
+    public Set<Service> services() {
+        return Nullsafe.nullsafe(ImmutableSet.<Service> of());
+    }
+
+}
---
+++ contract-examples/contract-cdi-juel-example/src/test/java/com/github/sebhoss/contract/example/CdiJuelInsuranceCompanyTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.example;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+import com.github.sebhoss.contract.annotation.JUEL;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.PaxExam;
+
+/**
+ * Test cases for the {@link InsuranceCompany}.
+ */
+@RunWith(PaxExam.class)
+@SuppressWarnings({ CompilerWarnings.NLS, CompilerWarnings.NULL })
+public class CdiJuelInsuranceCompanyTest {
+
+    /** Catches expected exceptions */
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Inject
+    @JUEL
+    @Nullable
+    private InsuranceCompany insurance;
+
+    /**
+     * Ensures that the insurance company can calculate payouts based on a positive damage input.
+     */
+    @Test
+    public void shouldAcceptPositiveDamages() {
+        final double result = insurance.calculateCover(10);
+
+        Assert.assertEquals(5.0, result, 0d);
+    }
+
+    /**
+     * Ensures that the insurance company cannot calculate payouts based on negative input.
+     */
+    @Test
+    public void shouldNotAcceptNegativeDamages() {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Reported damage must be positive!");
+
+        insurance.calculateCover(-10);
+    }
+
+    /**
+     * Ensures that the insurance company cannot calculate payouts based on high inputs.
+     */
+    @Test
+    public void shouldNotAcceptHighDamages() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("We won't pay that!");
+
+        insurance.calculateCover(5001);
+    }
+
+    /**
+     * Ensures that the insurance company cannot pay a high amount of money.
+     */
+    @Test
+    public void shouldNotPerformHighPayout() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("We can't pay that much!");
+
+        insurance.calculateCover(5000);
+    }
+
+}
