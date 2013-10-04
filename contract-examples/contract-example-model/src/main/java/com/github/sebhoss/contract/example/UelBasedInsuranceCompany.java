a714c7a0f8b2d7650a9911eeba2da7e979f383fc
Sebastian Hoß <mail@shoss.de>
2013-10-04 19:23:33 +0200

    Reference business methods
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- contract-examples/contract-example-model/src/main/java/com/github/sebhoss/contract/example/UelBasedInsuranceCompany.java
+++ contract-examples/contract-example-model/src/main/java/com/github/sebhoss/contract/example/UelBasedInsuranceCompany.java
@@ -17,9 +17,9 @@ class UelBasedInsuranceCompany extends AbstractInsuranceCompany {
 
     @Contract(preconditions = {
             @Clause(value = "${damage > 0}", message = "Reported damage must be positive!", exception = IllegalStateException.class),
-            @Clause(value = "${damage <= 5000}", message = "We won't pay that!") }, postconditions = {
+            @Clause(value = "${damage <= this.maximumReportableDamage}", message = "We won't pay that!") }, postconditions = {
             @Clause(value = "${return >= 0}", message = "We won't take any more!"),
-            @Clause(value = "${return <= 2000}", message = "We can't pay that much!") })
+            @Clause(value = "${return <= this.remainingMoney}", message = "We can't pay that much!") })
     @Override
     public double calculateCover(final double damage) {
         return damage * 0.5;
