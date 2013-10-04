8ebceb3d1d836e9f1aa288c902a79f061e08e1c7
Sebastian Hoß <mail@shoss.de>
2013-10-04 19:57:01 +0200

    Unique name for Guice-JavaScript example
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- contract-examples/contract-guice-js-example/src/test/java/com/github/sebhoss/contract/example/JavaScriptInsuranceCompanyTest.java
+++ contract-examples/contract-guice-js-example/src/test/java/com/github/sebhoss/contract/example/JavaScriptInsuranceCompanyTest.java
@@ -36,7 +36,7 @@ public class JavaScriptInsuranceCompanyTest {
      */
     @Before
     public void createCompany() {
-        final Injector injector = Guice.createInjector(new CompanyModule());
+        final Injector injector = Guice.createInjector(new GuiceJavaScriptModule());
         insurance = injector.getInstance(InsuranceCompany.class);
     }
 
