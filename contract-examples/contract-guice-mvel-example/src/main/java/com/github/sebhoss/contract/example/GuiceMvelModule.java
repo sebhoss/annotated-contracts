f96d53d61b180d0ed9380f9c5247921753f1aa7b
Sebastian Hoß <mail@shoss.de>
2013-10-04 19:55:22 +0200

    Unique name for Guice-MVEL example
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- contract-examples/contract-guice-mvel-example/src/main/java/com/github/sebhoss/contract/example/GuiceMvelModule.java
+++ contract-examples/contract-guice-mvel-example/src/main/java/com/github/sebhoss/contract/example/GuiceMvelModule.java
@@ -13,7 +13,7 @@ import com.google.inject.AbstractModule;
  * Simple Guice module which uses the default MVEL module to enable annotation-based contracts and creates an instance
  * of the domain model class.
  */
-public class CompanyModule extends AbstractModule {
+public class GuiceMvelModule extends AbstractModule {
 
     @Override
     protected void configure() {
