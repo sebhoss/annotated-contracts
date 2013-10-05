d78b4cca8c35baabafd040d53f1a0c579bbc8794
Sebastian Hoß <mail@shoss.de>
2013-10-05 14:50:31 +0200

    Split CDI-JavaScript integration
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-cdi/contract-cdi-javascript/src/main/java/com/github/sebhoss/contract/verifier/JavaScriptConfiguration.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier;
+
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Produces;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+import com.github.sebhoss.contract.annotation.Script;
+
+@SuppressWarnings(CompilerWarnings.STATIC_METHOD)
+class JavaScriptConfiguration {
+
+    @Produces
+    @Default
+    @SuppressWarnings(CompilerWarnings.NULL)
+    ScriptEngine javaScriptEngine() {
+        return new ScriptEngineManager().getEngineByName("JavaScript"); //$NON-NLS-1$
+    }
+
+    @Produces
+    @Default
+    ContractContextFactory contextFactory(final @Script ContractContextFactory scriptFactory) {
+        return scriptFactory;
+    }
+
+}
