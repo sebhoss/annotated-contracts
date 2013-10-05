be13e7871e9d72a8e87663f9c7bd748e4c1adf8f
Sebastian Hoß <mail@shoss.de>
2013-10-05 14:51:11 +0200

    Split CDI-SpEL integration
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-cdi/contract-cdi-spel/src/main/java/com/github/sebhoss/contract/verifier/SpELConfiguration.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier;
+
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Produces;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+import com.github.sebhoss.contract.annotation.SpEL;
+import com.github.sebhoss.contract.verifier.ContractContextFactory;
+
+class SpELConfiguration {
+
+    @Produces
+    @Default
+    @SuppressWarnings(CompilerWarnings.STATIC_METHOD)
+    ContractContextFactory contextFactory(final @SpEL ContractContextFactory spelFactory) {
+        return spelFactory;
+    }
+
+}
