f5428dfe38403a0d0deef094de76bcc07dfd6903
Sebastian Hoß <mail@shoss.de>
2013-10-05 14:50:40 +0200

    Split CDI-JEXL integration
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ contract-cdi/contract-cdi-jexl/src/main/java/com/github/sebhoss/contract/verifier/JexlConfiguration.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2012 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.contract.verifier;
+
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Produces;
+
+import org.apache.commons.jexl2.JexlEngine;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+import com.github.sebhoss.contract.annotation.JEXL;
+
+@SuppressWarnings(CompilerWarnings.STATIC_METHOD)
+class JexlConfiguration {
+
+    @Produces
+    @Default
+    JexlEngine jexlEngine() {
+        return new JexlEngine();
+    }
+
+    @Produces
+    @Default
+    ContractContextFactory contextFactory(final @JEXL ContractContextFactory jexlFactory) {
+        return jexlFactory;
+    }
+
+}
