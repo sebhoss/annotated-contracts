70aba1661d13c82650cd72f302ff6b0d2ccf7d87
Sebastian Hoß <mail@shoss.de>
2013-10-05 17:07:10 +0200

    Use shared test case for Guice tests
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- contract-examples/contract-guice-examples/contract-guice-ognl-example/src/test/java/com/github/sebhoss/contract/example/OgnlInsuranceCompanyTest.java
+++ contract-examples/contract-guice-examples/contract-guice-ognl-example/src/test/java/com/github/sebhoss/contract/example/OgnlInsuranceCompanyTest.java
@@ -6,30 +6,15 @@
  */
 package com.github.sebhoss.contract.example;
 
-import javax.annotation.Nullable;
-
-import com.github.sebhoss.common.annotation.CompilerWarnings;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 /**
- * Test cases for the {@link InsuranceCompany}.
+ * Test cases for a OGNL {@link InsuranceCompany}.
  */
-@SuppressWarnings({ CompilerWarnings.NLS, CompilerWarnings.NULL })
-public class OgnlInsuranceCompanyTest {
-
-    /** Catches expected exceptions */
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Nullable
-    private InsuranceCompany insurance;
+public class OgnlInsuranceCompanyTest extends InsuranceCompanyTest {
 
     /**
      * Creates a new insurance company for each test.
@@ -38,49 +23,6 @@ public class OgnlInsuranceCompanyTest {
     public void createCompany() {
         final Injector injector = Guice.createInjector(new GuiceOgnlModule());
         insurance = injector.getInstance(InsuranceCompany.class);
-    }
-
-    /**
-     * Ensures that the insurance company can calculate payouts based on a positive damage input.
-     */
-    @Test
-    public void shouldAcceptPositiveDamages() {
-        final double result = insurance.calculateCover(10);
-
-        Assert.assertEquals(5.0, result, 0d);
-    }
-
-    /**
-     * Ensures that the insurance company cannot calculate payouts based on negative input.
-     */
-    @Test
-    public void shouldNotAcceptNegativeDamages() {
-        thrown.expect(IllegalStateException.class);
-        thrown.expectMessage("Reported damage must be positive!");
-
-        insurance.calculateCover(-10);
-    }
-
-    /**
-     * Ensures that the insurance company cannot calculate payouts based on high inputs.
-     */
-    @Test
-    public void shouldNotAcceptHighDamages() {
-        thrown.expect(IllegalStateException.class);
-        thrown.expectMessage("We won't pay that!");
-
-        insurance.calculateCover(5001);
-    }
-
-    /**
-     * Ensures that the insurance company cannot pay a high amount of money.
-     */
-    @Test
-    public void shouldNotPerformHighPayout() {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("We can't pay that much!");
-
-        insurance.calculateCover(5000);
     }
 
 }
