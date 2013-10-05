d25daff8b622669cf11ae63c694ceea0345865a2
Sebastian Hoß <mail@shoss.de>
2013-10-05 17:18:43 +0200

    Use shared test case in Spring examples
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- contract-examples/contract-spring-examples/contract-spring-juel-example/src/test/java/com/github/sebhoss/contract/example/SpringJUELInsuranceCompanyTest.java
+++ contract-examples/contract-spring-examples/contract-spring-juel-example/src/test/java/com/github/sebhoss/contract/example/SpringJUELInsuranceCompanyTest.java
@@ -6,81 +6,25 @@
  */
 package com.github.sebhoss.contract.example;
 
-import javax.annotation.Nullable;
-
 import com.github.sebhoss.common.annotation.CompilerWarnings;
 
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
- * Test cases for the {@link InsuranceCompany}.
+ * Test cases for a JUEL {@link InsuranceCompany}.
  */
-@SuppressWarnings({ CompilerWarnings.NLS, CompilerWarnings.NULL, CompilerWarnings.RESOURCE })
-public class SpringJUELInsuranceCompanyTest {
-
-    /** Catches expected exceptions */
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Nullable
-    private InsuranceCompany insurance;
+public class SpringJUELInsuranceCompanyTest extends InsuranceCompanyTest {
 
     /**
      * Creates a new insurance company for each test.
      */
     @Before
+    @SuppressWarnings(CompilerWarnings.RESOURCE)
     public void createCompany() {
         final ApplicationContext context = new AnnotationConfigApplicationContext(SpringJuelConfiguration.class);
         insurance = context.getBean(InsuranceCompany.class);
-    }
-
-    /**
-     * Ensures that the insurance company can calculate payouts based on a positive damage input.
-     */
-    @Test
-    public void shouldAcceptPositiveDamages() {
-        final double result = insurance.calculateCover(10);
-
-        Assert.assertEquals(5.0, result, 0d);
-    }
-
-    /**
-     * Ensures that the insurance company cannot calculate payouts based on negative input.
-     */
-    @Test
-    public void shouldNotAcceptNegativeDamages() {
-        thrown.expect(IllegalStateException.class);
-        thrown.expectMessage("Reported damage must be positive!");
-
-        insurance.calculateCover(-10);
-    }
-
-    /**
-     * Ensures that the insurance company cannot calculate payouts based on high inputs.
-     */
-    @Test
-    public void shouldNotAcceptHighDamages() {
-        thrown.expect(IllegalStateException.class);
-        thrown.expectMessage("We won't pay that!");
-
-        insurance.calculateCover(5001);
-    }
-
-    /**
-     * Ensures that the insurance company cannot pay a high amount of money.
-     */
-    @Test
-    public void shouldNotPerformHighPayout() {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("We can't pay that much!");
-
-        insurance.calculateCover(5000);
     }
 
 }
