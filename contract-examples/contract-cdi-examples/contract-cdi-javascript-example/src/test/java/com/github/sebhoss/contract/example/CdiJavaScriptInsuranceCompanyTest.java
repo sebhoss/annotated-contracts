af347c6cfa52e1a9d3e3d0e5b053e8bc9d079ff1
Sebastian Hoß <mail@shoss.de>
2013-10-05 18:37:14 +0200

    Use shared test case
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- contract-examples/contract-cdi-examples/contract-cdi-javascript-example/src/test/java/com/github/sebhoss/contract/example/CdiJavaScriptInsuranceCompanyTest.java
+++ contract-examples/contract-cdi-examples/contract-cdi-javascript-example/src/test/java/com/github/sebhoss/contract/example/CdiJavaScriptInsuranceCompanyTest.java
@@ -9,13 +9,9 @@ package com.github.sebhoss.contract.example;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 
-import com.github.sebhoss.common.annotation.CompilerWarnings;
 import com.github.sebhoss.contract.annotation.Script;
 
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.PaxExam;
 
@@ -23,59 +19,18 @@ import org.ops4j.pax.exam.junit.PaxExam;
  * Test cases for the {@link InsuranceCompany}.
  */
 @RunWith(PaxExam.class)
-@SuppressWarnings({ CompilerWarnings.NLS, CompilerWarnings.NULL })
-public class CdiJavaScriptInsuranceCompanyTest {
-
-    /** Catches expected exceptions */
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
+public class CdiJavaScriptInsuranceCompanyTest extends InsuranceCompanyTest {
 
     @Inject
     @Script
     @Nullable
-    private InsuranceCompany insurance;
+    private InsuranceCompany injectedCompany;
 
     /**
-     * Ensures that the insurance company can calculate payouts based on a positive damage input.
+     * Bind the injected insurance company into the test
      */
-    @Test
-    public void shouldAcceptPositiveDamages() {
-        final double result = insurance.calculateCover(10);
-
-        Assert.assertEquals(5.0, result, 0d);
+    @Before
+    public void bind() {
+        insurance = injectedCompany;
     }
-
-    /**
-     * Ensures that the insurance company cannot calculate payouts based on negative input.
-     */
-    @Test
-    public void shouldNotAcceptNegativeDamages() {
-        thrown.expect(IllegalStateException.class);
-        thrown.expectMessage("Reported damage must be positive!");
-
-        insurance.calculateCover(-10);
-    }
-
-    /**
-     * Ensures that the insurance company cannot calculate payouts based on high inputs.
-     */
-    @Test
-    public void shouldNotAcceptHighDamages() {
-        thrown.expect(IllegalStateException.class);
-        thrown.expectMessage("We won't pay that!");
-
-        insurance.calculateCover(5001);
-    }
-
-    /**
-     * Ensures that the insurance company cannot pay a high amount of money.
-     */
-    @Test
-    public void shouldNotPerformHighPayout() {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("We can't pay that much!");
-
-        insurance.calculateCover(5000);
-    }
-
 }
--- contract-examples/contract-cdi-examples/contract-cdi-jexl-example/src/test/java/com/github/sebhoss/contract/example/CdiJexlInsuranceCompanyTest.java
+++ contract-examples/contract-cdi-examples/contract-cdi-jexl-example/src/test/java/com/github/sebhoss/contract/example/CdiJexlInsuranceCompanyTest.java
@@ -9,13 +9,9 @@ package com.github.sebhoss.contract.example;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 
-import com.github.sebhoss.common.annotation.CompilerWarnings;
 import com.github.sebhoss.contract.annotation.JEXL;
 
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.PaxExam;
 
@@ -23,59 +19,19 @@ import org.ops4j.pax.exam.junit.PaxExam;
  * Test cases for the {@link InsuranceCompany}.
  */
 @RunWith(PaxExam.class)
-@SuppressWarnings({ CompilerWarnings.NLS, CompilerWarnings.NULL })
-public class CdiJexlInsuranceCompanyTest {
-
-    /** Catches expected exceptions */
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
+public class CdiJexlInsuranceCompanyTest extends InsuranceCompanyTest {
 
     @Inject
     @JEXL
     @Nullable
-    private InsuranceCompany insurance;
+    private InsuranceCompany injectedCompany;
 
     /**
-     * Ensures that the insurance company can calculate payouts based on a positive damage input.
+     * Bind the injected insurance company into the test
      */
-    @Test
-    public void shouldAcceptPositiveDamages() {
-        final double result = insurance.calculateCover(10);
-
-        Assert.assertEquals(5.0, result, 0d);
-    }
-
-    /**
-     * Ensures that the insurance company cannot calculate payouts based on negative input.
-     */
-    @Test
-    public void shouldNotAcceptNegativeDamages() {
-        thrown.expect(IllegalStateException.class);
-        thrown.expectMessage("Reported damage must be positive!");
-
-        insurance.calculateCover(-10);
-    }
-
-    /**
-     * Ensures that the insurance company cannot calculate payouts based on high inputs.
-     */
-    @Test
-    public void shouldNotAcceptHighDamages() {
-        thrown.expect(IllegalStateException.class);
-        thrown.expectMessage("We won't pay that!");
-
-        insurance.calculateCover(5001);
-    }
-
-    /**
-     * Ensures that the insurance company cannot pay a high amount of money.
-     */
-    @Test
-    public void shouldNotPerformHighPayout() {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("We can't pay that much!");
-
-        insurance.calculateCover(5000);
+    @Before
+    public void bind() {
+        insurance = injectedCompany;
     }
 
 }
